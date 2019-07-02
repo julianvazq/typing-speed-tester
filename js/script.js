3a6753a520c1fe93ac95b0b1108b9dcc81e8865d
julianvazq <50368117+julianvazq@users.noreply.github.com>
2019-07-01 20:56:26 -0400

    Update js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 //Typing Speed Test
 const testWrapper = document.querySelector(".test-wrapper");
 const testArea = document.querySelector("#test-area");
-let oriText = document.querySelector("#origin-text p");
+const oriText = document.querySelector("#origin-text p");
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 const errorDisplay = document.querySelector(".errors span");
@@ -15,13 +15,18 @@ const hp = document.querySelector(".masthead .hp");
 const lotr = document.querySelector(".masthead .lotr");
 const backgroundWrapper = document.querySelector(".background-wrapper");
 const intro = document.querySelector(".intro");
+const textTitle = document.querySelector("h2");
 
-const texts = ["Following with his keen eyes the trail to the river, and then the river back towards the forest, Aragorn saw a shadow on the distant green, a dark swift-moving blur. He cast himself upon the ground and listened again intently. But Legolas stood beside him, shading his bright elven-eyes with his long slender hand, and he saw not a shadow, nor a blur, but the small figures of horsemen, many horsemen, and the glint of morning on the tips of their spears was like the twinkle of minute stars beyond the edge of mortal sight. Far behind them a dark smoke rose in thin curling threads.",
+const lotrTexts = ["Following with his keen eyes the trail to the river, and then the river back towards the forest, Aragorn saw a shadow on the distant green, a dark swift-moving blur. He cast himself upon the ground and listened again intently. But Legolas stood beside him, shading his bright elven-eyes with his long slender hand, and he saw not a shadow, nor a blur, but the small figures of horsemen, many horsemen, and the glint of morning on the tips of their spears was like the twinkle of minute stars beyond the edge of mortal sight. Far behind them a dark smoke rose in thin curling threads.",
                 "This is only a small selection of the assembled presents. Bilbo's residence had got rather cluttered up with things in the course of his long life. It was a tendency of hobbit-holes to get cluttered up: for which the custom of giving so many birthday- presents was largely responsible. Not, of course, that the birthday- presents were always new; there were one or two old mathoms of forgotten uses that had circulated all around the district; but Bilbo had usually given new presents, and kept those that he received. The old hole was now being cleared a little.",
                 "And all the host laughed and wept, and in the midst of their merriment and tears the clear voice of the minstrel rose like silver and gold, and all men were hushed. And he sang to them, now in the Elven-tongue, now in the speech of the West, until their hearts, wounded with sweet words, overflowed, and their joy was like swords, and they passed in thought out to regions where pain and delight flow together and tears are the very wine of blessedness.",
                 "Bilbo was very rich and very peculiar, and had been the wonder of the Shire for sixty years, ever since his remarkable disappearance and unexpected return. The riches he had brought back from his travels had now become a local legend, and it was popularly believed, whatever the old folk might say, that the Hill at Bag End was full of tunnels stuffed with treasure. And if that was not enough for fame, there was also his prolonged vigour to marvel at.",
                 "When every guest had been welcomed and was finally inside the gate, there were songs, dances, music, games, and, of course, food and drink. There were three official meals: lunch, tea, and dinner (or supper). But lunch and tea were marked chiefly by the fact that at those times all the guests were sitting down and eating together. At other times there were merely lots of people eating and drinking — continuously from elevenses until six-thirty, when the fireworks started.",
-                "Out of the wreck rose the Black Rider, tall and threatening, towering above her. With a cry of hatred that stung the very ears like venom he let fall his mace. Her shield was shivered in many pieces, and her arm was broken; she stumbled to her knees. He bent over her like a cloud, and his eyes glittered; he raised his mace to kill. But suddenly he too stumbled forward with a cry of bitter pain, and his stroke went wide, driving into the ground. "]
+                "Out of the wreck rose the Black Rider, tall and threatening, towering above her. With a cry of hatred that stung the very ears like venom he let fall his mace. Her shield was shivered in many pieces, and her arm was broken; she stumbled to her knees. He bent over her like a cloud, and his eyes glittered; he raised his mace to kill. But suddenly he too stumbled forward with a cry of bitter pain, and his stroke went wide, driving into the ground."]
+const hpTexts = ["But he understood at last what Dumbledore had been trying to tell him. It was, he thought, the difference between being dragged into the arena to face a battle to the death and walking into the arena with your head held high. Some people, perhaps, would say that there was little to choose between the two ways, but Dumbledore knew - and so do I, thought Harry, with a rush of fierce pride, and so did my parents - that there was all the difference in the world.",
+                "The bang was like a cannon blast, and the golden flames that erupted between them, at the dead center of the circle they had been treading, marked the point where the spells collided. Harry saw Voldemort’s green jet meet his own spell, saw the Elder Wand fly high, dark against the sunrise, spinning across the enchanted ceiling like the head of Nagini, spinning through the air toward the master it would not kill, who had come to take full possession of it at last.",
+                "He, Ron and Hermione were sitting at the very back of the Charms class with a table to themselves. They were supposed to be practicing the opposite of the Summoning Charm today - the Banishing Charm. Owing to the potential for nasty accidents when objects kept flying across the room, Professor Flitwick had given each student a stack of cushions on which to practice, the theory being that these wouldn’t hurt anyone if they went off target. It was a good theory, but it wasn’t working very well.",
+                "She Banished a cushion and it flew across the room and landed in the box they were all supposed to be aiming at. Harry looked at Hermione, thinking… it was true that Snape had saved his life once, but the odd thing was, Snape definitely loathed him, just as he’d loathed Harry’s father when they’d been at school together. Snape loved taking points from Harry, and had certainly never missed an opportunity to give him punishments, or even suggest he should be suspended from school."]
 var timer = [0, 0, 0, 0];
 var interval;
 var timerRunning = false;
@@ -30,24 +35,34 @@ var correctChar = 0;
 var typedEntry = 0;
 var wpm = 0;
 var pause = false;
-var rand = Math.floor(Math.random()*texts.length);
+var rand = Math.floor(Math.random()*lotrTexts.length);
+var hpTheme = false;
 
 function selectLeft() {
-    if (rand == 0) {
-        rand = texts.length-1;
+    if (hpTheme) {
+        if (rand == 0) {
+            rand = hpTexts.length-1;
+        } else {
+            rand--;
+        }
+        oriText.innerHTML = hpTexts[rand];
     } else {
-    rand--;
+        if (rand == 0) {
+            rand = lotrTexts.length-1;
+        } else {
+            rand--;
+        }
+        oriText.innerHTML = lotrTexts[rand];
     }
-    oriText.innerHTML = texts[rand];
 }
 
 function selectRight() {
-    if (rand == texts.length-1) {
+    if (rand == lotrTexts.length-1) {
         rand = 0;
     } else {
     rand++;
     }
-    oriText.innerHTML = texts[rand];
+    oriText.innerHTML = lotrTexts[rand];
 }
 
 
@@ -189,13 +204,22 @@ function changeTheme() {
         backgroundWrapper.style.backgroundPosition = "center";
         backgroundWrapper.style.backgroundRepeat = "no-repeat";
         intro.style.background = "#266464";
-
+        hpTheme = true;
+        textTitle.style.fontFamily = "'Cantata One', serif";
+        textTitle.innerHTML = "Harry Potter Excerpts";
+        rand = Math.floor(Math.random()*hpTexts.length);
+        oriText.innerHTML = hpTexts[rand];
     } else {
         backgroundWrapper.style.backgroundImage = "linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)), url(../images/lord-of-the-rings.jpg)";
         backgroundWrapper.style.backgroundSize = "cover";
         backgroundWrapper.style.backgroundPosition = "center";
         backgroundWrapper.style.backgroundRepeat = "no-repeat";
         intro.style.background = "#644726";
+        hpTheme = false;
+        textTitle.style.fontFamily = "'Old Standard TT', serif";
+        textTitle.innerHTML = "Lord of the Rings Excerpts";
+        rand = Math.floor(Math.random()*lotrTexts.length);
+        oriText.innerHTML = lotrTexts[rand];
     }
 }
 
@@ -206,7 +230,7 @@ resetButton.addEventListener("click", reset, false);
 leftArrow.addEventListener("click", selectLeft, false);
 rightArrow.addEventListener("click", selectRight, false);
 window.addEventListener('load', function (e) {
-    oriText.innerHTML = texts[rand];
+    oriText.innerHTML = lotrTexts[rand];
   })
 hp.addEventListener("click", changeTheme, false);
 lotr.addEventListener("click", changeTheme, false);
